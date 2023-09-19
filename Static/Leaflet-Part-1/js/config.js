f915efa8b6cfcae621fddfe847734812ea870504
AnaTipps <131827518+AnaTipps@users.noreply.github.com>
2023-09-19 11:09:28 +0100

    "Add your API KEY here"

--- Static/Leaflet-Part-1/js/config.js
+++ Static/Leaflet-Part-1/js/config.js
@@ -2,4 +2,4 @@
 // Follow steps bellow to access it:
 // Visit https://www.mapbox.com/ and create an account.
 // Visit the API Matrix section and copy your key below.
-const api_key = "pk.eyJ1IjoidGFsbGFudGo5NSIsImEiOiJjbGQwYmlicW0ydnZmM3BrNjhzcGxoMHVqIn0.NFVBr7AMOYS5BC5OwcXerA";
+const api_key = "YOUR API KEY HERE";
